67207bf961cab1f21eef6f3aeebc9c5a95665a8e
angular-tour-of-heroes <richardlimka@gmail.com>
2018-06-03 21:26:46 +0800

    update

--- src/App.vue
+++ src/App.vue
@@ -4,8 +4,11 @@
             <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
                 <h1>Routing</h1>
                 <hr>
-                <app-header></app-header>
-                <router-view></router-view>
+                <router-view name="header-top"></router-view>
+                <transition>
+                    <router-view></router-view>
+                </transition>
+                <router-view name="header=bottom"></router-view>
 
             </div>
         </div>
